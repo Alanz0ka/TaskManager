36ca2c13319079f461ecb0890fa259e5f209bb6e
Alanz0ka <jaas4@aluno.ifal.edu.br>
2024-09-12 18:13:16 -0300

    feat: Add models and configure admin access
    Adding the Task model and all its respective attributes, as well as adding it to the admin user.
    
    I added what allows creating migrations and the database.

--- Task/models.py
+++ Task/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    titulo = models.CharField(max_length=150, null=False)
+    descricao = models.TextField(null=True, blank=True, default='')
+    data = models.DateField(null=False)
+    horario = models.TimeField(null=True)
+
+    def __str__(self):
+        return self.titulo
+    
