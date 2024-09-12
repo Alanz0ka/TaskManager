22322d681fc818d48247eb12882e55e24d152e32
Alanz0ka <jaas4@aluno.ifal.edu.br>
2024-09-12 19:40:35 -0300

    feat: :sparkles: Add support for REST framework and add get method
    Add support for the REST framework by making the necessary adjustments to ensure the application functions as expected, and also add the GET method to test it.

--- Task/views.py
+++ Task/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Task
+from .serializers import TaskSerializer
 
-# Create your views here.
+class TaskAPIView(APIView):
+
+    def get(self, request):
+        task = Task.objects.all()
+        serializer = TaskSerializer(task, many=False)
+        return Response(serializer.data)
--- TaskManager/urls.py
+++ TaskManager/urls.py
@@ -1,9 +1,8 @@
-
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    #path('api/v1', include('Task.urls')),
+    path('api/v1/task', include('Task.urls')),
     path('admin/', admin.site.urls),
     path('auth/', include('rest_framework.urls')),
 ]
