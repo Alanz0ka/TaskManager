d43ab7554eb3cfad67cd9145128eb3b2439b6c27
Alanz0ka <jaas4@aluno.ifal.edu.br>
2024-09-12 22:11:32 -0300

    feat: :sparkles: Added PUT, UPDATE, DELETE, and PATCH methods, along with search by ID in GET request.
    Added support for PUT, PATCH, and DELETE methods to allow updating and deleting resources, along with the ability to search by ID in GET requests for more specific retrievals.

--- Task/urls.py
+++ Task/urls.py
@@ -3,4 +3,5 @@ from .views import TaskAPIView
 
 urlpatterns = [
     path('task/', TaskAPIView.as_view(), name='task'),
+    path('task/<int:pk>', TaskAPIView.as_view(), name='task'),
 ]
--- Task/views.py
+++ Task/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -7,11 +6,20 @@ from .serializers import TaskSerializer
 
 class TaskAPIView(APIView):
 
-    def get(self, request):
-        task = Task.objects.all()
-        serializer = TaskSerializer(task, many=True)
-        return Response(serializer.data)
-    
+    def get(self, request, pk=None):
+        if pk is not None:
+            
+            try:
+                task = Task.objects.get(pk=pk)
+                serializer = TaskSerializer(task)
+                return Response(serializer.data)
+            except Task.DoesNotExist:
+                return Response({'error': 'Task not found'}, status=status.HTTP_404_NOT_FOUND)
+        else:
+            tasks = Task.objects.all()
+            serializer = TaskSerializer(tasks, many=True)
+            return Response(serializer.data)
+
     def post(self, request):
         serializer = TaskSerializer(data=request.data)
         if serializer.is_valid():
@@ -19,4 +27,41 @@ class TaskAPIView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def put(self, request, pk):
+        try:
+            task = Task.objects.get(pk=pk)
+        except Task.DoesNotExist:
+            return Response({'error': 'Task not found'}, status=status.HTTP_404_NOT_FOUND)
         
+        serializer = TaskSerializer(task, data=request.data, partial=False)  
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request, pk):
+        
+        try:
+            task = Task.objects.get(pk=pk)
+        except Task.DoesNotExist:
+            return Response({'error': 'Task not found'}, status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = TaskSerializer(task, data=request.data, partial=True)  
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        
+        try:
+            task = Task.objects.get(pk=pk)
+        except Task.DoesNotExist:
+            return Response({'error': 'Task not found'}, status=status.HTTP_404_NOT_FOUND)
+        
+        task.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
